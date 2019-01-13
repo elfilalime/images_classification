70a03494ba81062c5c5a22df9425ae87c0ad6bf7
root <root@localhost.localdomain>
2019-01-14 00:52:21 +0100

    split data

--- data/split_data.py
+++ data/split_data.py
@@ -1,20 +1,39 @@
 import numpy as np
- 
+from minio import Minio
+from minio.error import ResponseError
+import time
 from pyspark import SparkContext
 from pyspark import SparkConf
+from tempfile import NamedTemporaryFile
  
 conf = SparkConf()
 conf.setMaster("spark://master:7077")
 conf.setAppName("NumpyMult")
 sc = SparkContext(conf=conf)
- 
-def mult(x):
-    y = np.array([2])
-    return x*y
- 
-x = np.arange(10000)
-distData = sc.parallelize(x)
- 
-results = distData.map(mult).collect()
- 
-print(results)
+
+
+def addToServer(image):
+	minioClient = Minio('minio:9000',access_key='minio',secret_key='minio123',secure=False)
+	ret = 0
+	try:
+		tempfile = NamedTemporaryFile()
+		np.save(tempfile, image)
+		tempfile.seek(0)
+
+		if not minioClient.bucket_exists('datas'):
+			minioClient.make_bucket('datas')
+
+		minioClient.fput_object('datas', str(time.time())+".npy", tempfile.name)
+		ret = time.time()
+	except:
+		ret = 0
+
+	return ret
+
+data = np.load('/home/train.npy')
+distData = sc.parallelize(data)
+
+s = distData.map(addToServer).collect()
+
+print(s)
+
